332e97d9d36c9b6192c085f6a0baf4e0c71e5e1f
yakash222000 <yakash222000@gmail.com>
2024-09-26 00:11:01 +0530

    resolved bugs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './component/Navbar/Navbar';
+import { ThemeProvider } from '@emotion/react';
+import { darkTheme } from './Theme/DarkTheme';
+import { CssBaseline } from '@mui/material';
+import {Home} from './component/Home/Home';
+import RestaurantDetails from './component/Restaurant/RestaurantDetails';
+import Cart from './component/Cart/Cart';
+import Profile from './component/Profile/Profile';
+import CustomerRoutes from './Routers/CustomerRoutes';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from './component/State/Authentication/Action';
+
+
 
 function App() {
+  const dispatch = useDispatch()
+  const jwt = localStorage.getItem("jwt")
+  const {auth} =useSelector((store) => store)
+  useEffect(()=>{
+    dispatch(getUser(auth.jwt || jwt))
+  },[auth.jwt, jwt, dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline/>
+    <CustomerRoutes/>
+    </ThemeProvider>
   );
 }
 
